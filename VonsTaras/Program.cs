2b7e94f2dcd540f38bf0016342d4daa78cbf0cf7
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-11-07 22:51:24 +0200

    test

--- VonsTaras/Program.cs
+++ VonsTaras/Program.cs
@@ -6,7 +6,7 @@ namespace VonsTaras
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Vons TaRAS!");
             
         }
     }
