9141f73740f5ea447219f40a8f7cf21240031ea1
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-11-14 17:20:42 +0200

    changes

--- VonsTaras/Program.cs
+++ VonsTaras/Program.cs
@@ -7,6 +7,8 @@ namespace VonsTaras
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Vons TaRAS!");
+
+            //push changes
             
         }
     }
